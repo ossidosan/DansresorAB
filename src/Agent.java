6f5db115975b77c92f4b9aeeedaf01b0f81472fa
p3lius <martin.petrelius@gmail.com>
2019-03-21 14:26:08 +0100

    vv

--- src/Agent.java
+++ src/Agent.java
@@ -1,4 +1,35 @@
 
 public class Agent {
+	
+	private String agentID; 
+	private String name;
+	private String phoneNumber; 
+	private String destination; 
+	
+	public String getAgentID() {
+		return agentID;
+	}
+	public void setAgentID(String agentID) {
+		this.agentID = agentID; 
+	}
+	public String getName() {
+		return name; 
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber (String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination (String destination) {
+		this.destination = destination;
+	}
+
 
 }
