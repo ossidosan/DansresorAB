ce356fdd6e0d5a899f8997f74a664e6cbcb424b2
LoveAnderberg <44205120+LoveAnderberg@users.noreply.github.com>
2019-03-21 15:31:27 +0100

    Final changes for today

--- src/TravelGuideRegister.java
+++ src/TravelGuideRegister.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
 
 public class TravelGuideRegister {
+	
+	private ArrayList<TravelGuide> TravelGuideList = new ArrayList<TravelGuide>();
+
+	public ArrayList<TravelGuide> getTravelGuideList() {
+		return TravelGuideList;
+	}
+
+	public void setTravelGuideList(ArrayList<TravelGuide> travelGuideList) {
+		TravelGuideList = travelGuideList;
+	}
+	
+	public void addTravelGuide (TravelGuide travelGuide) {
+		TravelGuideList.add(travelGuide);
+	}
+
+	
+	
 
 }
