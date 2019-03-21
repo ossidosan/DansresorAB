df5282c51fcd1199afc0f5ce6d7324c7bdd73a96
LoveAnderberg <44205120+LoveAnderberg@users.noreply.github.com>
2019-03-21 14:25:53 +0100

    Update TravelGuide.java

--- src/TravelGuide.java
+++ src/TravelGuide.java
@@ -1,4 +1,43 @@
 
 public class TravelGuide {
+	
+	private String travelGuideID;
+	private String name;
+	private String phoneNumber;
+	private String status;
+	
+	public void setTravelGuideID (String travelGuideID) {
+		this.travelGuideID = travelGuideID;
+	}
+	
+	public String getTravelGuideID() {
+		return travelGuideID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
 
 }
